4f3cd6be60b002bb584eda34ed2538a7e1da79b6
Derrick Hammer <derrick@derrickhammer.com>
2026-05-18 10:33:09 +0000

    refactor: convert to Caddy app module with Caddyfile global option
    Replace http.handlers module with cert_webhook app module activated via
    { cert_webhook } global option. Returns httpcaddyfile.App to register
    in config's apps section. All config via env vars, no JSON fields.
    
    For caddy-docker-proxy: caddy.cert_webhook= label
    For Caddyfile: { cert_webhook }
    
    Remove SSLStatusPending and SSLStatusIssuing (never emitted).
    Remove ServeHTTP passthrough and route registration.

--- module.go
+++ module.go
@@ -1,12 +1,25 @@
 package certwebhook
 
 import (
+	"encoding/json"
+
 	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
 )
 
 func init() {
 	caddy.RegisterModule(CertWebhookApp{})
+	httpcaddyfile.RegisterGlobalOption("cert_webhook", parseGlobalOption)
 }
 
 var _ caddy.App = (*CertWebhookApp)(nil)
+
+func parseGlobalOption(d *caddyfile.Dispenser, _ any) (any, error) {
+	d.Next()
+	return httpcaddyfile.App{
+		Name:  "cert_webhook",
+		Value: json.RawMessage(`{}`),
+	}, nil
+}
